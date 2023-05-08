c07316a08ad52db682a5d348b1cafd3a115cb05a
el_tio_dan <xddanieloow@gmail.com>
2023-05-07 18:28:37 -1000

    se agrego mas capitulos y se actualizo el componente home

--- src/Paginas/home/Home.jsx
+++ src/Paginas/home/Home.jsx
@@ -3,46 +3,42 @@ import "./Home.css"
 
 // componentes
 import Menu1 from '../../componentes/menus/Menu1';
-import Conseptos from '../../componentes/conseptos/conseptos';
 import Footer from '../../componentes/menus/Footer';
-import Opciones from '../../componentes/conseptos/opciones';
 import Capitulos from '../../aside';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <>
       <Menu1 />
       <Capitulos />
-      <main>
-        <h1>Apuntes programacion</h1>
-        <Conseptos texto={
-          "Esta pagina contiene apuntes de todo lo que yo he ido aprendiendo a lo largo del tiempo como desarrollador , a medida que vaya abanzando agregare mas apuntes a todo este sitio web cosas como c# , python u otros lenguajes a parte de javaScript . etc."
-        }/>
-
-          <Conseptos texto={`Ahora para programar de forma sensilla necesitamos de un editor de codigo aqui te dejo algunos bien conocidos
-
-Visual Studio Code: {vscode}
-Sublime Text: {sublime}`}
-enlaces={{
-  vscode: {text: "vscode", url: "https://code.visualstudio.com/"},
-  sublime: {text: "sublime" , url: "https://www.sublimetext.com/3"} 
-}} 
-targetBlank={true} />
-
-        <div>
-          <h2 className='title_home'>cursos</h2>
-          <div className='linksGome-content'>
-            <Opciones link={"/logica"} texto="logica"/>
-            <Opciones link={"/html"} texto="html"/>
-            <Opciones link={"/css"} texto="css"/>
-            <Opciones link={"/javaScript"} texto="javaScript"/>
-            <Opciones link={"/python"} texto="python"/>
-            <Opciones link={"/react"} texto="react"/>
-          </div>
-        </div>
+      
+      <main className='mainHome'>
+        <article>
+          <section>
+            <div className='title_consepto1'>
+              <h1 className='titleHome'>Apuntes programacion</h1>
+              <p className='conseptoHome'>
+                Esta página contiene apuntes de todo lo que yo he ido aprendiendo a lo largo del tiempo como desarrollador. A medida que vaya avanzando, agregaré más apuntes a todo este sitio web, cosas como C#, Python u otros lenguajes aparte de JavaScript, etc.
+              </p>
+            </div>
+          </section>
+          <section>
+            <div className='editoresCodigoContent'>
+              <h1 className='titleHome'>¡Comenzar!</h1>
+                <p className='edioresParrafo'>
+                  Ahora para programar de forma sensilla necesitamos de un editor de codigo aqui te dejo algunos bien conocidos.
+                  <br /> <br />
+                  Visual Studio Code: <Link target='_blank' to={"https://code.visualstudio.com/"}>vscode</Link> <br />
+                  Sublime Text: <Link target='_blank' to={"https://www.sublimetext.com/3"}>sublime</Link>
+                </p>
+              </div>
+          </section>
+        </article>
 
       </main>
       <Footer />
+      {/* <Capitulos /> */}
     </>
   );
 }
