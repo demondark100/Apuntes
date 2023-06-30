2cd4eceb4f666b2ddfed63a5f89db975b7f9e16e
el_tio_dan <xddanieloow@gmail.com>
2023-06-29 19:01:26 -1000

    pofin acabe la simulacion de editor de codigo en el proyecto

--- src/componentes/codificar/htCsJs.jsx
+++ src/componentes/codificar/htCsJs.jsx
@@ -3,15 +3,33 @@ import "./codificar.css";
 
 // iconos
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faJs, faCss3Alt, faHtml5 } from '@fortawesome/free-brands-svg-icons';
+import { faJs, faHtml5 } from '@fortawesome/free-brands-svg-icons';
 
 function HtCsJs() {
-  const codeJs = useRef(null);
 
+  // esta funcion se encarga de actualizar html
+  const [codeHtml, setCodeHtml] = useState(``);
+  const handleCodeChangeHtml = (event) => {
+    setCodeHtml(event.target.value);
+  };
+  const textIngresado = useRef(null);
+  const paginaHtml = useRef(null)
+  const actualizar=()=>{
+    paginaHtml.current.srcdoc = textIngresado.current.value
+  }
+
+
+  // toso esto renderiza la salida de la consola
+  const codeJs = useRef(null);
+  const showConsole = useRef(null); // esto sera para mostrar la consola en el dom en versiones mobiles.
+  
+  
   const [code, setCode] = useState('');
   const handleCodeChange = (event) => {
     setCode(event.target.value);
   };
+
+
 
   const [consoleOutput, setConsoleOutput] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
@@ -32,31 +50,27 @@ function HtCsJs() {
       setConsoleOutput(consoleOutput);
       setErrorMessage('');
     } catch (error) {
-      console.error(error);
+      console.error("");
       setConsoleOutput('');
       setErrorMessage('Hubo un error en tu código.');
     }
   };
 
+
+  // esto es para mostrar los editeores de texto
+
   const [html, setHtml] = useState(false);
-  const [css, setCss] = useState(false);
   const [javaScript, setJavaScript] = useState(false);
 
   const showHtml = () => {
     setHtml(!html);
-    setCss(false);
     setJavaScript(false);
   };
 
-  const showCss = () => {
-    setCss(!css);
-    setHtml(false);
-    setJavaScript(false);
-  };
+
 
   const showJavaScript = () => {
     setJavaScript(!javaScript);
-    setCss(false);
     setHtml(false);
   };
 
@@ -64,29 +78,53 @@ function HtCsJs() {
     <div className='contenedorTodoJsHtCs'>
       <div className='botondesCodeContent'>
         <button onClick={showHtml}><FontAwesomeIcon icon={faHtml5}/></button>
-        <button onClick={showCss}><FontAwesomeIcon icon={faCss3Alt}/></button>
         <button onClick={showJavaScript}><FontAwesomeIcon icon={faJs}/></button>
       </div>
-      {javaScript && (
-        <div className='contenedorJsCodificar'>
-          <textarea ref={codeJs} value={code} onChange={handleCodeChange} className='codificacionTexto'/>
-          <div className='compilarJs'>
-            <button onClick={handleRunCode}>ejecutar</button>
-            <button onClick={() => { codeJs.current.value = ""; }}>borrar todo</button>
-            <button>ver pagina</button>
+      
+      {
+        html && (
+          <div className='contenedorJsCodificar contenedorJsCodificarHtml'>
+            <div className='contenedorJsCodificar__contenedor'>
+              <textarea  
+                ref={textIngresado} 
+                value={codeHtml} 
+                onKeyUp={actualizar} 
+                className='codificacionTexto codificacionTextoHt' 
+                onChange={handleCodeChangeHtml}
+                defaultValue={"<style></style>"}
+              />
+            </div>
+            <iframe id='paginaCodigoHtml' ref={paginaHtml}></iframe>
           </div>
-          <div className="output">
-            <div className="console-output">
-              <h3>Salida de consola:</h3>
-              {errorMessage ? (
-                <p className="error-message">{errorMessage}</p>
-              ) : (
-                <pre>{consoleOutput}</pre>
-              )}
+        )
+      }
+
+      {
+        javaScript && (
+          <div className='contenedorJsCodificar'>
+            <div className='contenedorJsCodificar__contenedor'>
+              <textarea  ref={codeJs} value={code} onChange={handleCodeChange} className='codificacionTexto'/>
+              <div className='compilarJs'>
+                <button onClick={handleRunCode}>ejecutar</button>
+                <button onClick={() => { codeJs.current.value = ""; }}>borrar todo</button>
+              </div>
+            </div>
+
+            <div className="output" ref={showConsole}>
+              <div className="console-output">
+                <h3>Salida de consola:</h3>
+                {errorMessage ? (
+                  <p className="error-message">{errorMessage}</p>
+                ) : (
+                  <pre>{consoleOutput}</pre>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )
+      }
+
+
     </div>
   );
 }
