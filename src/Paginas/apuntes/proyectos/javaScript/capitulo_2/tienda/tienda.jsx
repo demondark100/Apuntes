85e6180d63d5f711ba5d26da5e67e732b821f8d2
el_tio_dan <xddanieloow@gmail.com>
2023-07-08 07:28:37 -1000

    por fin acabe el proyecto de los hjelados

--- src/Paginas/apuntes/proyectos/javaScript/capitulo_2/tienda/tienda.jsx
+++ src/Paginas/apuntes/proyectos/javaScript/capitulo_2/tienda/tienda.jsx
@@ -32,7 +32,7 @@ function Helado({
       <div
         style={{
           backgroundImage: `url(${imagen})`,
-          backgroundPosition: "center"
+          backgroundSize: "cover"
         }}
         className="contenedorHelado__content1"
       >
@@ -91,7 +91,10 @@ function Cap3ProyJsV3() {
     vainilla: [0,0,25],
     fresa: [0,0,20],
     chispitas: [0,0,36],
-    heladoFrito: [0,0,40]
+    heladoFrito: [0,0,40],
+    heladoCapuchino: [0,0,34],
+    chicle: [0,0,33],
+    platano: [0,0,29]
   });
 
 
@@ -117,13 +120,48 @@ function Cap3ProyJsV3() {
 
   const [showConfirmCompra, setShowConfirmCompra] = useState(false);
 
+    // este es el mensaje de que no alcanza el dinero.
+    const [mensaje1, setMensaje1] = useState(false); 
+
   const si =()=> {
     setconfirmarCompra(true)
     setShowConfirmCompra(false)
+
+    const modificar = { ...helados };
+    let nuevoDineroUsuario = dineroUsuario; // Almacena el valor inicial
+
+    for (let i in modificar) {
+      nuevoDineroUsuario -= modificar[i][1]; // Resta el valor del arreglo
+  
+      if (nuevoDineroUsuario < 0) {
+        nuevoDineroUsuario += modificar[i][1]; // Agrega nuevamente el valor del helado
+        modificar[i][0] = Math.floor(nuevoDineroUsuario / modificar[i][1]); // Calcula la cantidad de helados que puede comprar
+        setMensaje1(true)
+      }
+
+
+      modificar[i][0] = 0;
+      modificar[i][1] = 0;
+    }
+
+    setDineroUsuario(nuevoDineroUsuario); // Actualiza el estado con el nuevo valor
+    setHelados(modificar);
   }
   const no =()=> {
     setconfirmarCompra(false)
     setShowConfirmCompra(false)
+
+
+    const modificar = { ...helados };
+    let nuevoDineroUsuario = dineroUsuario; // Almacena el valor inicial
+
+    for (let i in modificar) {
+      modificar[i][0] = 0;
+      modificar[i][1] = 0;
+    }
+
+    setDineroUsuario(nuevoDineroUsuario); // Actualiza el estado con el nuevo valor
+    setHelados(modificar);
   }
 
   
@@ -131,47 +169,6 @@ function Cap3ProyJsV3() {
     setShowConfirmCompra(true)
   };
 
-
-  // este es el mensaje de que no alcanza el dinero.
-  const [mensaje1, setMensaje1] = useState(false); 
-  const [mensaje2, setMensaje2] = useState(false); // este es el mensaje de que no puede comprar mas helados hasta sierto tiempo.
-
-  useEffect(()=>{
-    const modificar = { ...helados };
-    let nuevoDineroUsuario = dineroUsuario; // Almacena el valor inicial
-    let dineroSuficiente = true; // Variable booleana para controlar si tiene suficiente dinero
-
-    
-    if (confirmarCompra == true) {
-      for (let i in modificar) {
-        nuevoDineroUsuario -= modificar[i][1]; // Resta el valor del arreglo
-    
-        if (nuevoDineroUsuario < 0) {
-          nuevoDineroUsuario += modificar[i][1]; // Agrega nuevamente el valor del helado
-          modificar[i][0] = Math.floor(nuevoDineroUsuario / modificar[i][1]); // Calcula la cantidad de helados que puede comprar
-          dineroSuficiente = false; // Actualiza la variable booleana
-          break; // Detiene el bucle
-        }
-    
-        modificar[i][0] = 0;
-        modificar[i][1] = 0;
-      }
-    }
-  
-    if (!dineroSuficiente) {
-      for (let i in modificar) {
-        modificar[i][0] = 0;
-        modificar[i][1] = 0;
-      }
-      setMensaje1(true)
-    }
-  
-    setDineroUsuario(nuevoDineroUsuario); // Actualiza el estado con el nuevo valor
-    setHelados(modificar);
-  },[confirmarCompra])
-  
-  
-  
 
 
 
@@ -225,86 +222,116 @@ cantidad de helados: helado n${"${i + 1}"}.
 // si hay mas helados para vender y todo xd.`}/>}/>
     
       <div className="Page">
-        
-        <div className={
-          showConfirmCompra ? 
-            "mostrarConfirmHelados"
-          :
-            "hideConfirmHelados"
-        }>
-          <ComparHelados 
-            si={si}
-            no={no}
-            setShowConfirmCompra={setShowConfirmCompra}
-          />
-        </div>
-
-        <div className={
-          mensaje1 ? 
-            "mostrarConfirmHelados"
-          :
-            "hideConfirmHelados"
-        }>
-          <Mensajes setMensaje1={setMensaje1} mensaje={"Usted no cuenta con dinero suficiente."}/>
-        </div>
-
-        <div className="contenedorTiendaHeladosProyJs">
-          <h1>Heladeria el helado</h1>
-          <div className="contenedorTiendaHeladosProyJs__presupuesto">
-            <p>su presupuesto es de: ${dineroUsuario}</p>
-          </div>
-          
-          <div className="componenteHelados">
-            <Helado 
-              texto={"helado de chocolate"}
-              precio={helados.chocolate[1]}
-              imagen={"https://th.bing.com/th/id/OIP.krNQLmf72s1tKYYGaHNw5gHaE8?w=304&h=203&c=7&r=0&o=5&dpr=1.1&pid=1.7"}
-              cantidad={helados.chocolate[0]}
-              incrementar={()=>incrementar("chocolate")}
-              decrementar={()=>decrementar("chocolate")}
-            />
-            <Helado 
-              texto={"helado de lucuma"}
-              precio={helados.lucuma[1]}
-              imagen={"https://th.bing.com/th/id/OIP.8c4t_rCPBZpMWIxB_3TefwHaE6?w=258&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"}
-              cantidad={helados.lucuma[0]}
-              incrementar={()=>incrementar("lucuma")}
-              decrementar={()=>decrementar("lucuma")}
-            />
-            <Helado 
-              texto={"helado de vainilla"}
-              precio={helados.vainilla[1]}
-              imagen={"https://th.bing.com/th/id/OIP.JLCndw61EmIn_4um0sttrwHaFz?w=220&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"}
-              cantidad={helados.vainilla[0]}
-              incrementar={()=>incrementar("vainilla")}
-              decrementar={()=>decrementar("vainilla")}
-            />
-            <Helado 
-              texto={"helado de fresa"}
-              precio={helados.fresa[1]}
-              imagen={"https://th.bing.com/th/id/OIP.iXlB5oP9GSFI3Q6rSX5XjAHaFm?w=221&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"}
-              cantidad={helados.fresa[0]}
-              incrementar={()=>incrementar("fresa")}
-              decrementar={()=>decrementar("fresa")}
-            />
-            <Helado 
-              texto={"helado de chispas de chocolate"}
-              precio={helados.chispitas[1]}
-              imagen={"https://th.bing.com/th/id/OIP.-YXLoEZo2GPx80d94IxbtwHaE7?w=259&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"}
-              cantidad={helados.chispitas[0]}
-              incrementar={()=>incrementar("chispitas")}
-              decrementar={()=>decrementar("chispitas")}
-            />
-            <Helado 
-              texto={"helado frito"}
-              precio={helados.heladoFrito[1]}
-              imagen={"https://th.bing.com/th/id/OIP.MSfSlEu5M1lpj_l39uwmIQHaEr?w=295&h=186&c=7&r=0&o=5&dpr=1.1&pid=1.7"}
-              cantidad={helados.heladoFrito[0]}
-              incrementar={()=>incrementar("heladoFrito")}
-              decrementar={()=>decrementar("heladoFrito")}
+        <div className="tiendaDeHeladosProyJsContenedorPrincipal">
+          <div className={
+            showConfirmCompra ? 
+              "mostrarConfirmHelados"
+            :
+              "hideConfirmHelados"
+          }>
+            <ComparHelados 
+              si={si}
+              no={no}
+              setShowConfirmCompra={setShowConfirmCompra}
             />
           </div>
-          <button onClick={comprar} className="comprarHelados">comprar</button>
+
+          <div className={
+            mensaje1 ? 
+              "mostrarConfirmHelados"
+            :
+              "hideConfirmHelados"
+          }>
+            <Mensajes setMensaje1={setMensaje1} mensaje={"Usted no cuenta con dinero suficiente."}/>
+          </div>
+
+          <div className="contenedorTiendaHeladosProyJs">
+            <h4>Heladeria el helado</h4>
+            <div className="contenedorTiendaHeladosProyJs__presupuesto">
+              <p>su presupuesto es de: ${dineroUsuario}</p>
+            </div>
+            
+            <div className="componenteHelados">
+              <Helado 
+                texto={"helado de chocolate"}
+                precio={helados.chocolate[1]}
+                imagen={"https://th.bing.com/th/id/OIP.krNQLmf72s1tKYYGaHNw5gHaE8?w=304&h=203&c=7&r=0&o=5&dpr=1.1&pid=1.7"}
+                cantidad={helados.chocolate[0]}
+                incrementar={()=>incrementar("chocolate")}
+                decrementar={()=>decrementar("chocolate")}
+              />
+              <Helado 
+                texto={"helado de lucuma"}
+                precio={helados.lucuma[1]}
+                imagen={"https://th.bing.com/th/id/OIP.8c4t_rCPBZpMWIxB_3TefwHaE6?w=258&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"}
+                cantidad={helados.lucuma[0]}
+                incrementar={()=>incrementar("lucuma")}
+                decrementar={()=>decrementar("lucuma")}
+              />
+              <Helado 
+                texto={"helado de vainilla"}
+                precio={helados.vainilla[1]}
+                imagen={"https://th.bing.com/th/id/OIP.JLCndw61EmIn_4um0sttrwHaFz?w=220&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"}
+                cantidad={helados.vainilla[0]}
+                incrementar={()=>incrementar("vainilla")}
+                decrementar={()=>decrementar("vainilla")}
+              />
+              <Helado 
+                texto={"helado de fresa"}
+                precio={helados.fresa[1]}
+                imagen={"https://th.bing.com/th/id/OIP.iXlB5oP9GSFI3Q6rSX5XjAHaFm?w=221&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"}
+                cantidad={helados.fresa[0]}
+                incrementar={()=>incrementar("fresa")}
+                decrementar={()=>decrementar("fresa")}
+              />
+              <Helado 
+                texto={"helado de chispas de chocolate"}
+                precio={helados.chispitas[1]}
+                imagen={"https://th.bing.com/th/id/OIP.-YXLoEZo2GPx80d94IxbtwHaE7?w=259&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"}
+                cantidad={helados.chispitas[0]}
+                incrementar={()=>incrementar("chispitas")}
+                decrementar={()=>decrementar("chispitas")}
+              />
+              <Helado 
+                texto={"helado frito"}
+                precio={helados.heladoFrito[1]}
+                imagen={"https://th.bing.com/th/id/OIP.MSfSlEu5M1lpj_l39uwmIQHaEr?w=295&h=186&c=7&r=0&o=5&dpr=1.1&pid=1.7"}
+                cantidad={helados.heladoFrito[0]}
+                incrementar={()=>incrementar("heladoFrito")}
+                decrementar={()=>decrementar("heladoFrito")}
+              />
+              <Helado 
+                texto={"helado de capuchino"}
+                precio={helados.heladoCapuchino[1]}
+                imagen={"https://th.bing.com/th/id/OIP.8eEEV1Yl9bjXsk-xUSgXNwHaHa?pid=ImgDet&w=193&h=193&c=7&dpr=1,1"}
+                cantidad={helados.heladoCapuchino[0]}
+                incrementar={()=>incrementar("heladoCapuchino")}
+                decrementar={()=>decrementar("heladoCapuchino")}
+              />
+              <Helado 
+                texto={"helado de chicle"}
+                precio={helados.chicle[1]}
+                imagen={"https://th.bing.com/th/id/OIP.D0MDeMJzg7FDUFIkXwD5nQHaHa?w=169&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"}
+                cantidad={helados.chicle[0]}
+                incrementar={()=>incrementar("chicle")}
+                decrementar={()=>decrementar("chicle")}
+              />
+              <Helado 
+                texto={"helado de platano"}
+                precio={helados.platano[1]}
+                imagen={"https://th.bing.com/th/id/OIP.XbmK4rK4kWPSq7HujTHJzAAAAA?w=211&h=208&c=7&r=0&o=5&dpr=1.1&pid=1.7"}
+                cantidad={helados.platano[0]}
+                incrementar={()=>incrementar("platano")}
+                decrementar={()=>decrementar("platano")}
+              />
+
+
+
+
+
+            </div>
+            <button onClick={comprar} className="comprarHelados">comprar</button>
+          </div>
         </div>
         
       </div>
