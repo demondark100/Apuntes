1b4566ffd42161aadc4e47f9b447f67f5c41486d
el_tio_dan <xddanieloow@gmail.com>
2024-04-22 06:52:10 -1000

    Se ha modificado los parametros de url en el curso de express se cambio la base de datos de ejemplo por uno que hara las cosas mas sensillas mas adelante.

--- src/Paginas/apuntes/node/capitulo 4/parametros.jsx
+++ src/Paginas/apuntes/node/capitulo 4/parametros.jsx
@@ -12,28 +12,26 @@ function ParametrosExNode() {
         "lenguage": "JavaScropt",
         "codigo": `const express = require("express");
 const app = express();
-// base de datos,
-const base = require("./base.js");
-
+const {cursos} = require("./cursos.js");
 app.get("/",(req,res)=>{
-  res.send(JSON.stringify(base.animes));
+  res.send(JSON.stringify("pagina principal."));
 })
-app.get("/api/animes",(req,res)=>{
-  res.send(JSON.stringify(base.animes))
+app.get("/api/cusos",(req,res)=>{
+  res.send(JSON.stringify(cursos))
 })
-app.get("/api/animes/shonen/:anime",(req,res)=>{
-  const anime = req.params.anime;
-  const entregar = base.animes.shonen.filter(animeData=>animeData.titulo == anime)
+app.get("/api/cursos/programacion/:lenguaje",(req,res)=>{
+  const lenguaje = req.params.lenguaje;
+  const entregar = cursos.programacion.filter(curso=>curso.lenguaje == lenguaje)
   if (entregar.length === 0) {
-    return res.status(404).send(\`No se ha encontrado ${"${anime}"}.\`)
+    return res.status(404).send(\`No se ha encontrado ${"${lenguaje}"}.\`)
   }
   res.send(JSON.stringify(entregar));
 })
-app.get("/api/animes/isekai/:anime",(req,res)=>{
-  const anime = req.params.anime;
-  const entregar = base.animes.isekai.filter(animeData=>animeData.titulo == anime)
+app.get("/api/cursos/matematica/:tema",(req,res)=>{
+  const tema = req.params.tema;
+  const entregar = cursos.matematica.filter(curso=>curso.tema == tema)
   if (entregar.length === 0) {
-    return res.status(404).send(\`No se ha encontrado ${"${anime}"}.\`)
+    return res.status(404).send(\`No se ha encontrado ${"${tema}"}.\`)
   }
   res.send(JSON.stringify(entregar));
 })
@@ -47,88 +45,85 @@ app.listen(PUERTO,()=>{
       <main>
         <h1>Parametros url</h1>
         <Conseptos texto={`Ahora si queremos obtener datos de algo en especifico seria muy tedioso crear una peticion para cada cosa , usaremos la base de datos anterior como ejemplo.`}/>
-        <JavaScropt codigo={`const animes = {
-  "shonen":[
+        <JavaScropt codigo={`const cursos = {
+  programacion: [
     {
-      "titulo": "dragon ball",
-      "id": 1,
-      "vistas": 380000,
-      "destaca": "sayajins"
+      id: 1,
+      titulo: "aprende python",
+      lenguaje: "python",
+      vistas: 15789,
+      nivel: "basico"
     },
     {
-      "titulo": "Naruto",
-      "id": 2,
-      "vistas": 347914,
-      "destaca": "ninja"
+      id: 2,
+      titulo: "python intermedio",
+      lenguaje: "python",
+      vistas: 14789,
+      nivel: "medio"
     },
     {
-      "titulo": "one piece",
-      "id": 3,
-      "vistas": 347954,
-      "destaca": "piratas"
+      id: 3,
+      titulo: "aprende jabaScript",
+      lenguaje: "javaScript",
+      vistas: 16789,
+      nivel: "basico"
     }
   ],
-  "isekai":[
+  matematica: [
     {
-      "titulo": "re zero",
-      "id": 1,
-      "vistas": 248751,
-      "destaca": "gore"
+      id: 1,
+      titulo: "aprende calculo",
+      tema: "calculo",
+      vistas: 13487,
+      nivel: "basico"
     },
     {
-      "titulo": "mushoku tensei",
-      "id": 2,
-      "vistas": 257489,
-      "destaca": "ecchi"
-    },    
-    {
-      "titulo": "tate no yuusha",
-      "id": 3,
-      "vistas": 247687,
-      "destaca": "historia"
+      id: 2,
+      titulo: "aprende algebra",
+      tema: "algebra",
+      vistas: 12487,
+      nivel: "intermedio"
     }
   ]
 }
 
-module.exports.animes = animes;`}/>
+module.exports.cursos = cursos;`}/>
         <Conseptos texto={`En este caso tenemos solo dos generos de anime "shonen" e "isekai" , ahora que tal si el cliente quiere los datos de "one piece" deberiamos hacer esto¿.`}/>
-        <JavaScropt codigo={`app.get("/api/anumes/shonen/one piece",()=>funcion)`}/>
-        <Conseptos texto={`¿Y si ahora el cliente quiere informacion de dragon ball?`}/>
-        <JavaScropt codigo={`app.get("/api/anumes/shonen/dragon ball",()=>funcion)`}/>
-        <Conseptos texto={`O ¿y si ahora el cliente quiere informacion de "naruto"?`}/>
-        <JavaScropt codigo={`app.get("/api/anumes/shonen/naruto",()=>funcion)`}/>
-        <Conseptos texto={`Como te das cuenta tenemos que hacer varias peticiones en la categoria de "shonen" y es probable que tengan la misma funcion de dar la informacion lo cual seria muy tedioso de hacer peticion a cada ruta , ademas imagina que tienes una base de dato mucho mas grande con muchos mas animes seria poco practico repetir el mismo codigo para cada ruta por eso estan los parametros.`}/>
+        <JavaScropt codigo={`app.get("/api/cursos/programacion/python",()=>funcion)`}/>
+        <Conseptos texto={`¿Y si ahora el cliente quiere informacion de otro lenguaje como javaScript?`}/>
+        <JavaScropt codigo={`app.get("/api/cursos/programacion/javaScript",()=>funcion)`}/>
+        <Conseptos texto={`O ¿y si ahora el cliente quiere informacion de "c"?`}/>
+        <JavaScropt codigo={`app.get("/api/cursos/programacion/c",()=>funcion)`}/>
+        <Conseptos texto={`Como te das cuenta tenemos que hacer varias peticiones en la categoria de "programacion" y es probable que tengan la misma funcion de dar la informacion lo cual seria muy tedioso de hacer peticion a cada ruta , ademas imagina que tienes una base de dato mucho mas grande con muchos mas lenguajes de programacion seria poco practico repetir el mismo codigo para cada ruta por eso estan los parametros.`}/>
         <Conseptos texto={`Ahora para implementar parametros en la url necesitamos poner ":parametro" en el codigo donde se hara la peticion.`}/>
-        <Sintaxis codigo={`app.get("/animes/shonen/:parametro",()=>funcion)`}/>
+        <Sintaxis codigo={`app.get("/api/cursos/:parametro",()=>funcion)`}/>
         <h2>api con parametros</h2>
         <Conseptos texto={`Usaremos la base de datos del inicio para hacer esta api.`}/>
         <JavaScropt codigo={`const express = require("express");
 const app = express();
 // base de datos,
-const base = require("./base.js");
-
-
+const {cursos} = require("./cursos.js");
 
 app.get("/",(req,res)=>{
-  res.send(JSON.stringify(base.animes));
+  res.send(JSON.stringify("pagina principal."));
 })
 
-app.get("/api/animes",(req,res)=>{
-  res.send(JSON.stringify(base.animes))
+app.get("/api/cusos",(req,res)=>{
+  res.send(JSON.stringify(cursos))
 })
-app.get("/api/animes/shonen/:anime",(req,res)=>{
-  const anime = req.params.anime;
-  const entregar = base.animes.shonen.filter(animeData=>animeData.titulo == anime)
+app.get("/api/cursos/programacion/:lenguaje",(req,res)=>{
+  const lenguaje = req.params.lenguaje;
+  const entregar = cursos.programacion.filter(curso=>curso.lenguaje == lenguaje)
   if (entregar.length === 0) {
-    return res.status(404).send(\`No se ha encontrado ${"${anime}"}.\`)
+    return res.status(404).send(\`No se ha encontrado ${"${lenguaje}"}.\`)
   }
   res.send(JSON.stringify(entregar));
 })
-app.get("/api/animes/isekai/:anime",(req,res)=>{
-  const anime = req.params.anime;
-  const entregar = base.animes.isekai.filter(animeData=>animeData.titulo == anime)
+app.get("/api/cursos/matematica/:tema",(req,res)=>{
+  const tema = req.params.tema;
+  const entregar = cursos.matematica.filter(curso=>curso.tema == tema)
   if (entregar.length === 0) {
-    return res.status(404).send(\`No se ha encontrado ${"${anime}"}.\`)
+    return res.status(404).send(\`No se ha encontrado ${"${tema}"}.\`)
   }
   res.send(JSON.stringify(entregar));
 })
@@ -142,43 +137,39 @@ app.listen(PUERTO,()=>{
         <Sintaxis codigo={`const express = require("express");
 const app = express();
 // base de datos,
-const base = require("./base.js");
+const {cursos} = require("./cursos.js");
 
-
-
-
-app.get("/api/animes",(req,res)=>{
+app.get("/api/cursos",(req,res)=>{
   // devolver toda la base de datos.
-  res.send(JSON.stringify(base.animes))
+  res.send(JSON.stringify({cursos}));
 })
 
-// ":anime" sera el parametro en este caso el titulo.
-app.get("/api/animes/shonen/:anime",(req,res)=>{
+// ":lenguaje" sera el parametro en este caso el lenguaje de programacion.
+app.get("/api/cursos/programacion/:lenguaje",(req,res)=>{
   // req.params es un arreglo que contiene los objetos del parametro indicado
-  // en este caso animes porque en la ruta especificamos que el parametro sera ":anime"
-  const anime = req.params.anime;
-  // aqui filtramos si el parametro de la url es igual al titulo del anime
+  // en este caso lenguaje porque en la ruta especificamos que el parametro sera ":lenguaje"
+  const lenguaje = req.params.lenguaje;
+  // aqui filtramos si el parametro de la url es igual al titulo del lenguaje
   // que queremos tener informacion , esto retornara un arreglo.
-  const entregar = base.animes.shonen.filter(animeData=>animeData.titulo == anime)
+  const entregar = cursos.programacion.filter(curso=>curso.lenguaje == lenguaje)
   // aqui verificamos si la longitud de "entregar" es 0 significa que no hay resultados.
   if (entregar.length === 0) {
     // retornamos el estado de error e indicamos un mensaje al cliente.
-    return res.status(404).send(\`No se ha encontrado ${"${anime}"}.\`)
+    return res.status(404).send(\`No se ha encontrado ${"${lenguaje}"}.\`)
   }
-  // retornamos la informacion del anime que busca el cliente.
+  // retornamos la informacion del lenguaje que busca el cliente.
   res.send(JSON.stringify(entregar));
 })
 
 // codigo sin  comentarios.
-app.get("/api/animes/isekai/:anime",(req,res)=>{
-  const anime = req.params.anime;
-  const entregar = base.animes.isekai.filter(animeData=>animeData.titulo == anime)
+app.get("/api/cursos/matematica/:tema",(req,res)=>{
+  const tema = req.params.tema;
+  const entregar = cusos.matematica.filter(curso=>curso.tema == tema);
   if (entregar.length === 0) {
     return res.status(404).send(\`No se ha encontrado ${"${anime}"}.\`)
   }
   res.send(JSON.stringify(entregar));
 })
-
 
 const PUERTO = process.env.PORT || 8080;
 app.listen(PUERTO,()=>{
@@ -187,31 +178,31 @@ app.listen(PUERTO,()=>{
 
         <h2>2 o mas parametros</h2>
         <Conseptos texto={`Tambien podemos poner mas parametros en la url.`}/>
-        <Sintaxis codigo={`app.get("/api/animes/:parametro1/parametro2",()=>funcion)`}/>
+        <Sintaxis codigo={`app.get("/api/cursos/:parametro1/:parametro2",()=>funcion)`}/>
         <Conseptos texto={`En este ejercicio usaremos la base de datos del principio tambien.`}/>
         <JavaScropt codigo={`const express = require("express");
 const app = express();
 // base de datos,
-const base = require("./base.js");
+const {cursos} = require("./cursos.js");
 
-app.get("/api/animes/shonen/:titulo/:destaca",(req,res)=>{
-  const titulo = req.params.titulo;
-  const destaca = req.params.destaca;
-  const informacion = base.animes.shonen.filter(animes=>animes.titulo === titulo && animes.destaca === destaca)
-  if (informacion.length === 0) {
-    return res.status(404).send(\`No se encontro ${"${titulo}"} o ${"${titulo}"} no destaca por ${"${destaca}"}\`);
+app.get("/api/cursos/programacion/:lenguaje/:nivel",(req,res)=>{
+  const lenguaje = req.params.lenguaje;
+  const nivel = req.params.nivel;
+  const entregar = cursos.programacion.filter(curso=>curso.lenguaje === lenguaje && curso.nivel === nivel);
+  if (entregar.length === 0) {
+    return res.status(404).send(\`No se encontro ${"${lenguaje}"} o el nivel ${"${nivel}"} no esta disponible.\`);
   }
-  res.send(JSON.stringify(informacion))
+  res.send(JSON.stringify(entregar));
 })
 
-app.get("/api/animes/isekai/:titulo/:destaca",(req,res)=>{
-  const titulo = req.params.titulo;
-  const destaca = req.params.destaca;
-  const informacion = base.animes.isekai.filter(animes=>animes.titulo === titulo && animes.destaca === destaca);
-  if (informacion.length === 0) {
-    return res.status(404).send(\`No se ha encontrado ${"${titulo}"} o ${"${titulo}"} no destaca por ${"${destaca}"}\`);
+app.get("/api/cursos/matematica/:tema/:nivel",(req,res)=>{
+  const tema = req.params.tema;
+  const nivel = req.params.nivel;
+  const entregar = cursos.matematica.filter(curso=>curso.tema === tema && curso.nivel === nivel);
+  if (entregar.length === 0) {
+    return res.status(404).send(\`No se ha encontrado ${"${tema}"} o el nivel ${"${nivel}"} no esta disponible.\`);
   }
-  res.send(JSON.stringify(informacion));
+  res.send(JSON.stringify(entregar));
 })
 
 const PUERTO = process.env.PORT || 8080;
