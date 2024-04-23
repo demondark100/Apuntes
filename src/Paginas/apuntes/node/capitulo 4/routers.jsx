34f56004ac2cb3c1c685f9ab13871bf1afb18837
el_tio_dan <xddanieloow@gmail.com>
2024-04-22 18:06:24 -1000

    Se modifico la base de datos y se adapto el curso de reuters en express

--- src/Paginas/apuntes/node/capitulo 4/routers.jsx
+++ src/Paginas/apuntes/node/capitulo 4/routers.jsx
@@ -13,19 +13,15 @@ function RoutersNode() {
         "lenguage": "JavaScropt",
         "codigo": `const express = require("express");
 const app = express();
-const base = require("./base.js");
-
+const {cursos} = require("./cursos.js");
 const ruta = express.Router();
-app.use("/animes/api",ruta)
+app.use("/api/cursos",ruta)
 
-ruta.get("/",(req,res)=>{
-  res.send("ruta principal");
+ruta.get("/programacion",(req,res)=>{
+  return res.send(JSON.stringify(cursos.programacion));
 })
-ruta.get("/shonen",(req,res)=>{
-  return res.send(JSON.stringify(base.animes.shonen))
-})
-ruta.get("/isekai",(req,res)=>{
-  return res.send(JSON.stringify(base.animes.isekai))
+ruta.get("/matematica",(req,res)=>{
+  return res.send(JSON.stringify(cursos.matematica))
 })
 const PUERTO = process.env.PORT || 8080;
 app.listen(PUERTO,()=>{
@@ -34,114 +30,99 @@ app.listen(PUERTO,()=>{
       }]}/>
       <main>
         <h1>Routers</h1>
-        <Conseptos texto={`Al momento de trabjar con las ruta hay veces que se repite mucho una ruta principal en la funcion lo cual puede hacer que el codigo sea poco legible por eso llego "Router" para solucionar ese problema.`}/>
-        <JavaScropt codigo={`app.get("/animes/api/generos",()=>funcion);
-app.get("/animes/api/generos/shonen",()=>funcion);
-app.get("/animes/api/generos/isekai",()=>funcion);
-app.get("/animes/api/generos/gore",()=>funcion);`}/>
-        <Conseptos texto={`Como nos damos cuenta la ruta "/animes/api/generos"`}/>
+        <Conseptos texto={`Al momento de trabjar con las rutas hay veces que se repite mucho una ruta principal en la funcion lo cual puede hacer que el codigo sea poco legible por eso llego "Router" para solucionar ese problema.`}/>
+        <JavaScropt codigo={`app.get("/api/cursos/programacion",()=>funcion);
+app.get("/api/cursos/matematica",()=>funcion);
+app.get("/api/cursos/ciencia",()=>funcion);
+app.get("/api/cursos/historia",()=>funcion);`}/>
+        <Conseptos texto={`Como nos damos cuenta la ruta "/api/cursos/" se repite demasiado.`}/>
         <Conseptos texto={`Ahora imagina que programaremos algo para una ruta mucho mas larga seria muy larga las rutas por eso podemos acortar eso con "Router".`}/>
-        <JavaScropt codigo={`const rutaPrincipal = express.Router(); // aqyu llamamos a la funcion Router con express.
-app.use("/animes/api/generos",rutaPrincipal); 
+        <JavaScropt codigo={`const rutaPrincipal = express.Router(); // aqui llamamos a la funcion Router con express.
+app.use("/api/cursos",rutaPrincipal); 
 // El primer parametro es para poner la ruta que se repite.
 // El segundo parametro es para poner la variable que llama a la funcion "Router".
 
 // en vez de usar "app" usaremos la variable "rutaPrincipal" porque esa variable guarda la ruta que se repite.
-rutaPrincipal.get("/",()=>{ // al usar "rutaPrincipal" y poner "/" mo estamos haciendo una ruta desde 0 sino que estamos partiendo desde "/animes/api/generos"
+rutaPrincipal.get("/",()=>{ // al usar "rutaPrincipal" y poner "/" mo estamos haciendo una ruta desde 0 sino que estamos partiendo desde "/api/cursos"
   // funcion
 })
-rutaPrincipal.get("/shonen",(req,res)=>{
+rutaPrincipal.get("/programacion",(req,res)=>{
   // funcion
 })
-rutaPrincipal.get("/isekai",(req,res)=>{
-  // funcion
-})
-rutaPrincipal.get("/gore",(req,res)=>{
+rutaPrincipal.get("/matematica",(req,res)=>{
   // funcion
 })`}/>
         <Conseptos texto={`Ahora haremos una api para usando "Router".`}/>
-        <JavaScropt codigo={``}/>
         <JavaScropt codigo={`const express = require("express");
 const app = express();
 // base de datos,
-const base = require("./base.js");
-
+const {cusos} = require("./cursos.js");
 const ruta = express.Router();
-app.use("/animes/api",ruta)
+app.use("/api/cursos",ruta)
 
 ruta.get("/",(req,res)=>{
-  res.send("ruta principal");
+  res.send(JSON.stringify(cursos));
 })
 
-ruta.get("/shonen",(req,res)=>{
-  return res.send(JSON.stringify(base.animes.shonen))
+ruta.get("/programacion",(req,res)=>{
+  return res.send(JSON.stringify(cursos.programacion));
 })
-ruta.get("/isekai",(req,res)=>{
-  return res.send(JSON.stringify(base.animes.isekai))
+ruta.get("/matematica",(req,res)=>{
+  return res.send(JSON.stringify(cursos.matematica));
 })
 
 const PUERTO = process.env.PORT || 8080;
 app.listen(PUERTO,()=>{
     console.log(\`El servidor esta escuchando en el puerto ${"${PUERTO}"}...\`);
 })`}/>
-        <Txt codigo={`# peticiones http
-
-### 
-GET http://localhost:8080/animes/api
+        <Txt codigo={`###
+GET http://localhost:8080/api/cursos/programacion
 # resultado
-# ruta principal
-
-
-###
-GET http://localhost:8080/animes/api/shonen
-# resultado
-
 [
   {
-    "titulo": "dragon ball",
     "id": 1,
-    "vistas": 380000,
-    "destaca": "sayajins"
+    "titulo": "aprende python",
+    "lenguaje": "python",
+    "vistas": 15789,
+    "nivel": "basico"
   },
   {
-    "titulo": "naruto",
     "id": 2,
-    "vistas": 347914,
-    "destaca": "ninjas"
+    "titulo": "python intermedio",
+    "lenguaje": "python",
+    "vistas": 14789,
+    "nivel": "medio"
   },
   {
-    "titulo": "one piece",
     "id": 3,
-    "vistas": 347954,
-    "destaca": "piratas"
+    "titulo": "aprende jabaScript",
+    "lenguaje": "javaScript",
+    "vistas": 16789,
+    "nivel": "basico"
   }
 ]
 
 
 ###
-GET http://localhost:8080/animes/api/isekai
+GET http://localhost:8080/api/cursos/matematica
 # resultado
-
 [
   {
-    "titulo": "re zero",
     "id": 1,
-    "vistas": 248751,
-    "destaca": "gore"
+    "titulo": "aprende calculo",
+    "tema": "calculo",
+    "vistas": 13487,
+    "nivel": "basico"
   },
   {
-    "titulo": "mushoku tensei",
     "id": 2,
-    "vistas": 257489,
-    "genero": "ecchi"
-  },
-  {
-    "titulo": "tate no yuusha",
-    "id": 3,
-    "vistas": 247687,
-    "genero": "historia"
+    "titulo": "python algebra",
+    "tema": "algebra",
+    "vistas": 12487,
+    "nivel": "intermedio"
   }
-]`}/>
+]
+`}/>
       </main>
       <Footer />
     </>
