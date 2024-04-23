3ca0746bfba37a1082f056a90af4fb013d971dc2
el_tio_dan <xddanieloow@gmail.com>
2024-04-22 14:27:08 -1000

    Se agrego la nueva base al curso de parametros de busqueda en express y se le agrego un resumen

--- src/Paginas/apuntes/node/capitulo 4/paramQuery.jsx
+++ src/Paginas/apuntes/node/capitulo 4/paramQuery.jsx
@@ -3,94 +3,105 @@ import Footer from "../../../../componentes/menus/Footer";
 import Sintaxis from "../../../../componentes/lenguajes/Sintaxis";
 import JavaScropt from "../../../../componentes/lenguajes/JavaScript";
 import Txt from "../../../../componentes/lenguajes/Txt";
+import Resumenes from "../../../../componentes/resumenes/resumenes";
 
 function ParametrosQueryNode() {
   return (  
     <>
+      <Resumenes contenido={[{
+        "mensaje": `Con estos parametros de busqueda el cliente tiene mas formas de buscar algun contenido en especifico o hacer algo con la api.`,
+        "lenguage": `JavaScropt`,
+        "codigo": `app.get("/api/cursos/programacion",(req,res)=>{
+  if(req.query.ordenar === "vistas"){
+    return res.send(JSON.stringify(cursos.programacion.sort((a,b)=>a.vistas - b.vistas)))
+  }
+})`
+      },
+      {
+        "lenguage": "Txt",
+        "codigo": `###
+http://localhost:8080/api/cursos/programacion?ordenar=vistas`
+      }]}/>
       <main>
         <h1>parametros query</h1>
         <Conseptos texto={`Los parametros query siempre van al final de la url , estos siempre llevaran un "?" recordemos como era.`}/>
         <Sintaxis codigo={`GET http://localhost:3000/contenido/:parametros?clave=valor&clave=valor`}/>
         <Conseptos texto={`Empezaremos haciendo nuestra base de datos , sera la misma que hemos estado usando.`}/>
-        <JavaScropt codigo={`const animes = {
-  "shonen":[
+        <JavaScropt codigo={`const cursos = {
+  programacion: [
     {
-      "titulo": "dragon ball",
-      "id": 1,
-      "vistas": 380000,
-      "destaca": "sayajins",
+      id: 1,
+      titulo: "aprende python",
+      lenguaje: "python",
+      vistas: 15789,
+      nivel: "basico"
     },
     {
-      "titulo": "naruto",
-      "id": 2,
-      "vistas": 347914,
-      "destaca": "ninjas"
+      id: 2,
+      titulo: "python intermedio",
+      lenguaje: "python",
+      vistas: 14789,
+      nivel: "medio"
     },
     {
-      "titulo": "one piece",
-      "id": 3,
-      "vistas": 347954,
-      "destaca": "piratas"
+      id: 3,
+      titulo: "aprende jabaScript",
+      lenguaje: "javaScript",
+      vistas: 16789,
+      nivel: "basico"
     }
   ],
-  "isekai":[
+  matematica: [
     {
-      "titulo": "re zero",
-      "id": 1,
-      "vistas": 248751,
-      "destaca": "gore"
+      id: 1,
+      titulo: "aprende calculo",
+      tema: "calculo",
+      vistas: 13487,
+      nivel: "basico"
     },
     {
-      "titulo": "mushoku tensei",
-      "id": 2,
-      "vistas": 257489,
-      "genero": "ecchi"
-    },    
-    {
-      "titulo": "tate no yuusha",
-      "id": 3,
-      "vistas": 247687,
-      "genero": "historia"
+      id: 2,
+      titulo: "aprende algebra",
+      tema: "algebra",
+      vistas: 12487,
+      nivel: "intermedio"
     }
   ]
 }
 
-module.exports.animes = animes;`}/>
+module.exports.cursos = cursos;`}/>
         <Conseptos texto={`Ahora para trabajar con los parametros query debemos recordar que son un objeto de clave y valor.`}/>
         <Sintaxis codigo={`{"clave":"valor"}`}/>
         <Conseptos texto={`Para acceder a esa "clave: valor" debemos usar el objeto query y el valor que queremos acceder.`}/>
         <Sintaxis codigo={`"req.query.valor del parametro query";`}/>
         <Conseptos texto={`Imaginemos que esta es la peticion.`}/>
-        <Conseptos texto={`/animes/api/shonen?clave=valor`}/>
+        <Conseptos texto={`/cursos/programacion/?clave=valor`}/>
         <JavaScropt codigo={`Para acceder al objeto debemos hacer esto.`}/>
         <JavaScropt codigo={`console.log(req.query.clave);
 // resultado
 // "valor"`}/>
-        <Conseptos texto={`Es hora de hacer un ejercico en este caso ordenaremos de los animes menos vistos a los mas vistos en cada genero de anime.`}/>
+        <Conseptos texto={`Es hora de hacer un ejercico en este caso ordenaremos de los cursos menos vistos a los mas vistos en cada lenguage de programacion o tema de matematica.`}/>
         <JavaScropt codigo={`const express = require("express");
 const app = express();
 // base de datos,
-const base = require("./base.js");
+const {cursos} = require("./cursos.js");
 
-
-app.get("/animes/api/shonen",(req,res)=>{
+app.get("/api/cursos/programacion",(req,res)=>{
   // en este caso el cliente pasa un parametro query llamado ordenar con el valor vistas
   // ?ordenar=vistas
   // {ordenar: "vistas"}
 
   if (req.query.ordenar === "vistas") {
-    return res.send(JSON.stringify(base.animes.shonen.sort((a,b)=>a.vistas - b.vistas)))
+    return res.send(JSON.stringify(cursos.programacion.sort((a,b)=>a.vistas - b.vistas)))
   }
-  res.send(JSON.stringify(base.animes.shonen))
+  res.send(JSON.stringify(cursos.programacion))
 })
 
-
-app.get("/animes/api/isekai",(req,res)=>{
+app.get("/api/cursos/matematica",(req,res)=>{
   if (req.query.ordenar === "vistas") {
-    return res.send(JSON.stringify(base.animes.isekai.sort((a,b)=>a.vistas - b.vistas)))
+    return res.send(JSON.stringify(cursos.matematica.sort((a,b)=>a.vistas - b.vistas)))
   }
-
-  res.send(JSON.stringify(base.animes.isekai));
+  res.send(JSON.stringify(cursos.matematica));
 })
 
 
@@ -99,53 +110,49 @@ app.listen(PUERTO,()=>{
     console.log(\`El servidor esta escuchando en el puerto ${"${PUERTO}"}...\`);
 })`}/>
         <Txt codigo={`###
-GET http://localhost:8080/animes/api/shonen?ordenar=vistas
-
+GET http://localhost:8080/api/cursos/programacion?ordenar=vistas
 # resultado
 [
   {
-    "titulo": "naruto",
     "id": 2,
-    "vistas": 347914,
-    "destaca": "ninjas"
+    "titulo": "python intermedio",
+    "lenguaje": "python",
+    "vistas": 14789,
+    "nivel": "medio"
   },
   {
-    "titulo": "one piece",
-    "id": 3,
-    "vistas": 347954,
-    "destaca": "piratas"
-  },
-  {
-    "titulo": "dragon ball",
     "id": 1,
-    "vistas": 380000,
-    "destaca": "sayajins"
+    "titulo": "aprende python",
+    "lenguaje": "python",
+    "vistas": 15789,
+    "nivel": "basico"
+  },
+  {
+    "id": 3,
+    "titulo": "aprende jabaScript",
+    "lenguaje": "javaScript",
+    "vistas": 16789,
+    "nivel": "basico"
   }
 ]
 
+
 ###
-GET http://localhost:8080/animes/api/isekai?ordenar=vistas
-
-# resultado
-
+GET http://localhost:8080/api/cursos/matematica?ordenar=vistas
 [
   {
-    "titulo": "tate no yuusha",
-    "id": 3,
-    "vistas": 247687,
-    "genero": "historia"
-  },
-  {
-    "titulo": "re zero",
-    "id": 1,
-    "vistas": 248751,
-    "destaca": "gore"
-  },
-  {
-    "titulo": "mushoku tensei",
     "id": 2,
-    "vistas": 257489,
-    "genero": "ecchi"
+    "titulo": "python algebra",
+    "tema": "algebra",
+    "vistas": 12487,
+    "nivel": "intermedio"
+  },
+  {
+    "id": 1,
+    "titulo": "aprende calculo",
+    "tema": "calculo",
+    "vistas": 13487,
+    "nivel": "basico"
   }
 ]`}/>
         <Conseptos texto={`Si te fijas ordena los animes de los menos vistos a los mas vistos.`}/>
