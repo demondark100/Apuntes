0ea68d36f9ca12d7f50ddf2baecb79612c1bdb8b
el_tio_dan <xddanieloow@gmail.com>
2024-04-29 16:57:49 -1000

    Se agregaron cosas extra de node.js no se hara resumen de esto.

--- src/Paginas/apuntes/node/capitulo 4/extra.jsx
+++ src/Paginas/apuntes/node/capitulo 4/extra.jsx
@@ -1,7 +1,26 @@
+import Footer from "../../../../componentes/menus/Footer";
+import Conseptos from "../../../../componentes/conseptos/conseptos";
+import JavaScropt from "../../../../componentes/lenguajes/JavaScript";
+
 function ExtraNode(){
   return (
     <>
-    
+      <main>
+        <h1>Cosas extra de express.</h1>
+        <Conseptos texto={`Ahora veremos cosas extra de express que seran muy utiles.`}/>
+        <h2>json</h2>
+        <Conseptos texto={`En vez de usar "res.send(contenido)" podriamos usar un metodo para asegurarnos de siempre mandar un archivo json al cliente.`}/>
+        <JavaScropt codigo={`ruta.get("/",(req,res)=>{
+  res.json(base);
+})`}/>
+        <h2>Consumir</h2>
+        <Conseptos texto={`Ahora vamos a configurar nuestro servidor para que pueda ser consumido por el front-end.`}/>
+        <JavaScropt codigo={`app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});`}/>
+      </main>
+      <Footer />
     </>
   )
 }
